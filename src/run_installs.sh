eacfcb4eb413d0c2317b312739085bc42cc53108
thabotr <1831661@students.wits.ac.za>
2020-08-22 11:17:44 +0200

    Resolving file not found in travis ci

--- src/run_installs.sh
+++ src/run_installs.sh
@@ -1,7 +1,7 @@
 #this scripts automatically install all the depencies for anroid marking
 #components java-8
 
-source ./dependencies_install_lib.sh
+source ./src/dependencies_install_lib.sh
 
 #install java 8
 install_java #<travis alrady has openjdk8 so for now we'll comment this
