28c39534b8782945b0454cfb322962d45689779e
thabotr <1831661@students.wits.ac.za>
2020-09-06 00:09:31 +0200

    PREV

--- src/dependencies_install_lib.sh
+++ src/dependencies_install_lib.sh
@@ -323,8 +323,7 @@ create_default_avd2()
 	if ! $( mkdir -p $avd_dir && chmod a+rw $avd_dir ); then
 		return 1
 	fi
-	find $MARKER_TOOLS	
+	ls -l $MARKER_TOOLS
 	#create avd
 	avdmanager create avd -n $avd_name -c "512M" -k $package -g "default" -b "x86_64" -p $avd_dir -d 19
-	find $MARKER_TOOLS
 }
