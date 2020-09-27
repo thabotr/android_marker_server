89171457a688fd7608ada12552245a13ed132eea
thabotr <1831661@students.wits.ac.za>
2020-09-27 16:04:29 +0200

    Testing avd managent and android setups only.

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,6 @@
 #manually run the java command
 #./bats/bin/bats tests/
 #./bats/bin/bats tests/test_online_emulator.bats -T
-./bats/bin/bats tests/test_gradle_operations.bats -T
+#./bats/bin/bats tests/test_gradle_operations.bats -T
+./bats/bin/bats tests/test_android_environment.bats -T
+./bats/bin/bats tests/test_avd_management.bats -T
