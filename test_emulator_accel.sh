8b6f3757b979cb0b9381530e54a79ae4d620d66e
thabotr <1831661@students.wits.ac.za>
2020-11-22 03:55:32 +0200

    Testing if x86 emulator can be run on travis with -no-accel option.

---
+++ test_emulator_accel.sh
@@ -0,0 +1,15 @@
+wget https://dl.google.com/android/repository/commandlinetools-linux-6858069_latest.zip
+
+
+mkdir cmdline-tools
+
+unzip -q -d cmdline-tools commandline*.zip
+
+rm commandline*.zip
+
+export PATH=$PATH:"$( echo cmdline-tools/**/bin)":"$( echo cmdline-tools/tools)"
+
+echo yes | sdkmanager --install emulator > /dev/null
+
+#for now list versions so we know what build tools to get
+
