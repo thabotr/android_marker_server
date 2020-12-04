dd1bc92280b276b1557e6e18d850e96456c6e830
thabotr <1831661@students.wits.ac.za>
2020-12-04 11:11:16 +0200

    Listsing emulator

--- test_emulator_accel.sh
+++ test_emulator_accel.sh
@@ -18,3 +18,5 @@ yes | sdkmanager --licenses > /dev/null
 sdkmanager --install "system-images;android-28;default;x86_64"
 
 echo no | avdmanager create avd -f -n emulator1 -c "512M" -k "system-images;android-28;default;x86_64"
+
+avdmanager list avds
