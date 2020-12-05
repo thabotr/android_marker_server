863084d81443cd995c92aca76f9b53f75bdc3d2c
thabotr <1831661@students.wits.ac.za>
2020-12-05 14:51:37 +0200

    PREV trying to start emulator

--- test_emulator_accel.sh
+++ test_emulator_accel.sh
@@ -1,5 +1,26 @@
-wget https://dl.google.com/android/repository/commandlinetools-linux-6858069_latest.zip
+export MARKER_TOOLS="$HOME/marker_tools"
+#sdk location
+export ANDROID_HOME="$MARKER_TOOLS/android_sdk"
+#avd location
+export AVD_HOME="$MARKER_TOOLS/.android/avd"
 
+mkdir -p $MARKER_TOOLS $ANDROID_HOME $AVD_HOME
+
+cd $MARKER_TOOLS
+
+#move android avds directory
+cp -R "$HOME/.android" -t $MARKER_TOOLS
+rm -rf "$HOME/.android"
+
+export ANDROID_EMULATOR_HOME="$MARKER_TOOLS/.android"
+export AANDROID_AVD_HOME=$AVD_HOME
+
+export AVD_LOGS="$ANDROID_AVD_HOME/logs"
+mkdir -p $AVD_LOGS
+
+
+#install sdk
+wget https://dl.google.com/android/repository/commandlinetools-linux-6858069_latest.zip
 
 mkdir cmdline-tools
 
@@ -7,7 +28,11 @@ unzip -q -d cmdline-tools commandline*.zip
 
 rm commandline*.zip
 
-export PATH=$PATH:"$( echo cmdline-tools/**/bin)":"$( echo cmdline-tools/tools)"
+mkdir android_sdk
+mv cmdline-tools -t android_sdk
+
+export PATH=$PATH:"$( echo android_sdk/cmdline-tools/**/bin)":"$( echo android_sdk/cmdline-tools/tools)"
+#sdk location
 
 #for now list versions so we know what build tools to get
 yes | sdkmanager --licenses > /dev/null
@@ -16,8 +41,6 @@ sdkmanager "emulator" > /dev/null #"tools" "platform-tools" > /dev/null
 
 #add emulator directory to path so we can use bin emulator
 export PATH=$PATH:emulator/
-#define ANDROID_SDK_ROOT as required
-export ANDROID_SDK_ROOT="cmdline-tools"
 
 #install emulator package
 sdkmanager --install "system-images;android-28;default;x86_64"
