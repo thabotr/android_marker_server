ce1fcce4299cb25db2be0c0076692f871d26e644
thabotr <1831661@students.wits.ac.za>
2020-12-08 03:55:30 +0200

    PREV

--- test_emulator_accel.sh
+++ test_emulator_accel.sh
@@ -1,20 +1,20 @@
 export MARKER_TOOLS="$HOME/marker_tools"
 #sdk location
-export ANDROID_HOME="$MARKER_TOOLS/android_sdk/cmdline-tools"
-export ANDROID_SDK_ROOT=$ANDROID_HOME
-#avd location
-export AVD_HOME="$MARKER_TOOLS/.android/avd"
+export ANDROID_SDK_ROOT="$MARKER_TOOLS/android_sdk"
 
-mkdir -p $MARKER_TOOLS $ANDROID_HOME $AVD_HOME
+#avd location
+export ANDROID_AVD_HOME="$MARKER_TOOLS/.android/avd"
+
+mkdir -p $MARKER_TOOLS $ANDROID_SDK_ROOT $ANDROID_AVD_HOME
 
 cd $MARKER_TOOLS
 
-#move android avds directory
-cp -R "$HOME/.android" -t $MARKER_TOOLS
-rm -rf "$HOME/.android"
-
 export ANDROID_EMULATOR_HOME="$MARKER_TOOLS/.android"
 export ANDROID_AVD_HOME=$AVD_HOME
+
+#move android avds directory
+#cp -R "$HOME/.android" -t $MARKER_TOOLS
+#rm -rf "$HOME/.android"
 
 export AVD_LOGS="$ANDROID_AVD_HOME/logs"
 mkdir -p $AVD_LOGS
@@ -22,24 +22,24 @@ mkdir -p $AVD_LOGS
 #install sdk
 wget https://dl.google.com/android/repository/commandlinetools-linux-6858069_latest.zip
 
-unzip -q -d $ANDROID_HOME commandline*.zip
+unzip -q -d $ANDROID_SDK_ROOT commandline*.zip
 rm *.zip
-
-export PATH=$PATH:"$ANDROID_HOME/cmdline-tools/bin" #sdk location
-
-#for now list versions so we know what build tools to get
-yes | sdkmanager --licenses > /dev/null
-
-sdkmanager "emulator" "platform-tools"> /dev/null #platform tools for adb
-#add emulator directory to path so we can use bin emulator
-export PATH=$PATH:"$MARKER_TOOLS/android_sdk/emulator":"$MARKER_TOOLS/android_sdk/platform-tools"
-
-#install emulator package
-sdkmanager --install "system-images;android-28;default;x86_64"
-
-pwd
 find .
-
-echo no | avdmanager create avd -f -n emulator1 -c "512M" -k "system-images;android-28;default;x86_64"
-
-emulator @emulator1 -gpu swiftshader_indirect -memory 512 -no-window -no-boot-anim -no-audio -no-snapshot -camera-front none -camera-back none -selinux permissive -no-qt -wipe-data -no-accel
+#export PATH=$PATH:"$ANDROID_HOME/cmdline-tools/bin" #sdk location
+#
+##for now list versions so we know what build tools to get
+#yes | sdkmanager --licenses > /dev/null
+#
+#sdkmanager "emulator" "platform-tools"> /dev/null #platform tools for adb
+##add emulator directory to path so we can use bin emulator
+#export PATH=$PATH:"$MARKER_TOOLS/android_sdk/emulator":"$MARKER_TOOLS/android_sdk/platform-tools"
+#
+##install emulator package
+#sdkmanager --install "system-images;android-28;default;x86_64"
+#
+#pwd
+#find .
+#
+#echo no | avdmanager create avd -f -n emulator1 -c "512M" -k "system-images;android-28;default;x86_64"
+#
+#emulator @emulator1 -gpu swiftshader_indirect -memory 512 -no-window -no-boot-anim -no-audio -no-snapshot -camera-front none -camera-back none -selinux permissive -no-qt -wipe-data -no-accel
