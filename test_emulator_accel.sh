b255c511bfe8a48cea1b894d1c37b720276b66d7
thabotr <1831661@students.wits.ac.za>
2020-12-11 19:52:14 +0200

    PREV

--- test_emulator_accel.sh
+++ test_emulator_accel.sh
@@ -3,13 +3,13 @@ export MARKER_TOOLS="$HOME/marker_tools"
 export ANDROID_SDK_ROOT="$MARKER_TOOLS/android-sdk-linux"
 
 #avd location
-export ANDROID_AVD_HOME="$MARKER_TOOLS/.android/avd"
+export ANDROID_AVD_HOME="$ANDROID_SDK_ROOT/.android/avd"
 
 mkdir -p $MARKER_TOOLS $ANDROID_SDK_ROOT $ANDROID_AVD_HOME
 
 cd $MARKER_TOOLS
 
-export ANDROID_EMULATOR_HOME="$MARKER_TOOLS/.android"
+export ANDROID_EMULATOR_HOME="$ANDROID_SDK_ROOT/.android"
 
 #move android avds directory
 #cp -R "$HOME/.android" -t $MARKER_TOOLS
@@ -40,7 +40,5 @@ tree ~/
 sdkmanager --install "system-images;android-28;default;x86_64"
 
 echo no | avdmanager create avd -f -n emulator1 -c "512M" -k "system-images;android-28;default;x86_64" -p $ANDROID_AVD_HOME
-
-find .
 
 emulator @emulator1 -gpu swiftshader_indirect -memory 512 -no-window -no-boot-anim -no-audio -no-snapshot -camera-front none -camera-back none -selinux permissive -no-qt -wipe-data -no-accel
