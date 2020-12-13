05c77377c39d1b1f3f4bbedeff516e73afc96552
thabotr <1831661@students.wits.ac.za>
2020-12-13 19:54:52 +0200

    PREV

--- test_emulator_accel.sh
+++ test_emulator_accel.sh
@@ -1,9 +1,9 @@
-export MARKER_TOOLS="$HOME/marker_tools"
-#sdk location
-export ANDROID_SDK_ROOT="$MARKER_TOOLS/android_sdk"
-export ANDROID_SDK_HOME="$MARKER_TOOLS/android_avd"
-export ANDROID_EMULATOR_HOME="$ANDROID_SDK_HOME/.android"
-export ANDROID_AVD_HOME="$ANDROID_EMULATOR_HOME/avd" #avd data files
+source set_env_vars.bash
+set_var_marker_tools
+set_var_android_sdk_root
+set_var_android_sdk_home
+set_var_android_emulator_home
+set_var_android_avd_home
 
 #make relevant directories
 mkdir -p $MARKER_TOOLS $ANDROID_SDK_ROOT $ANDROID_AVD_HOME
@@ -25,19 +25,23 @@ mv tools -t "$ANDROID_SDK_ROOT/cmdline-tools"
 #getting latest sdk
 $ANDROID_SDK_ROOT/cmdline-tools/tools/bin/sdkmanager --install "cmdline-tools;latest"
 rm -rf $ANDROID_SDK_ROOT/cmdline-tools/tools  #removing tools directory in orde rto use latest sdk tools, NB: mayb break as new sdk updates and changes arrive
-export PATH=$PATH:"$ANDROID_SDK_ROOT/cmdline-tools/latest/bin"
+export_latest_tools_bin
 
 sdkmanager --install "platform-tools" > /dev/null
 sdkmanager --install "build-tools;30.0.3" > /dev/null
 sdkmanager --install "emulator" > /dev/null
 
-export PATH=$PATH:"$ANDROID_SDK_ROOT/emulator":"$ANDROID_SDK_ROOT/platform-tools"
+export_amulator
+export_platform_tools
+
 #install image package
 sdkmanager --install "system-images;android-28;default;x86_64"
 sdkmanager --install "platforms;android-28" #solves SDK installation not found problem
 
 echo no | avdmanager create avd -f -n emulator1 -c "512M" -k "system-images;android-28;default;x86_64"
 
-emulator @emulator1 -gpu swiftshader_indirect -memory 512 -no-window -no-boot-anim -no-audio -no-snapshot -camera-front none -camera-back none -selinux permissive -no-qt -wipe-data -no-accel -sysdir "$ANDROID_SDK_ROOT/system-images/android-28/default/x86_64" #sysdir solves broken AVD path problem as by default the sys images are searched for in emulator dir
+emulator @emulator1 -gpu swiftshader_indirect -memory 512 -no-window -no-boot-anim -no-audio -no-snapshot -camera-front none -camera-back none -selinux permissive -no-qt -wipe-data -no-accel -sysdir "$ANDROID_SDK_ROOT/system-images/android-28/default/x86_64" 2>&1 > /dev/null & #sysdir solves broken AVD path problem as by default the sys images are searched for in emulator dir
+
+adb wait-for-device
 
 adb devices
