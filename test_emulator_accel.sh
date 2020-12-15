817f207c12f1add7d6b2e7a49dab9fef8ab3de43
thabotr <1831661@students.wits.ac.za>
2020-12-16 01:45:31 +0200

    PREV

--- test_emulator_accel.sh
+++ test_emulator_accel.sh
@@ -11,12 +11,12 @@ mkdir -p $MARKER_TOOLS $ANDROID_SDK_ROOT $ANDROID_AVD_HOME
 #change into tools directory
 cd $MARKER_TOOLS
 
-find ../
+find .
 
-source ../sdk_lib.bash || ( echo "ERROR! SDK library could not be sourced to install SDK." && exit 1 )
+source sdk_lib.bash || ( echo "ERROR! SDK library could not be sourced to install SDK." && exit 1 )
 get_commandline_tools
 
-source ../emulator_manager_lib.bash || ( echo "ERROR! Emulator manager library could not be sourced." && exit 1 )
+source emulator_manager_lib.bash || ( echo "ERROR! Emulator manager library could not be sourced." && exit 1 )
 install_emulator_image "28" "default" "x86_64"
 
 echo no | avdmanager create avd -f -n emulator1 -c "512M" -k "system-images;android-28;default;x86_64"
@@ -28,3 +28,4 @@ adb wait-for-device
 adb devices
 
 adb -s emulator-5554 shell getprop
+
