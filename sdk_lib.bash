8dda0813eabb21fb3d193b9b4c34f57e82c194b9
thabotr <1831661@students.wits.ac.za>
2020-12-16 01:16:31 +0200

    Functionalizing emulator management tools.

---
+++ sdk_lib.bash
@@ -0,0 +1,25 @@
+get_commandline_tools()
+{
+	#get commandline tools
+	wget https://dl.google.com/android/repository/commandlinetools-linux-6858069_latest.zip
+
+	unzip -q commandline*.zip
+	rm *.zip
+	mkdir "$ANDROID_SDK_ROOT/cmdline-tools"
+	mv cmdline-tools tools
+	mv tools -t "$ANDROID_SDK_ROOT/cmdline-tools"
+
+	#accept licenses
+	( yes || true ) | $ANDROID_SDK_ROOT/cmdline-tools/tools/bin/sdkmanager --licenses > /dev/null
+
+	#getting latest sdk
+	$ANDROID_SDK_ROOT/cmdline-tools/tools/bin/sdkmanager --install "cmdline-tools;latest"
+	rm -rf $ANDROID_SDK_ROOT/cmdline-tools/tools  #removing tools directory in orde rto use latest sdk tools, NB: mayb break as new sdk updates and changes arrive
+	export_latest_tools_bin
+	sdkmanager --install "platform-tools" > /dev/null
+	sdkmanager --install "build-tools;30.0.3" > /dev/null
+	sdkmanager --install "emulator" > /dev/null
+	
+	export_emulator
+	export_platform_tools
+}
--- test_emulator_accel.sh
+++ test_emulator_accel.sh
@@ -8,35 +8,14 @@ set_var_android_avd_home
 #make relevant directories
 mkdir -p $MARKER_TOOLS $ANDROID_SDK_ROOT $ANDROID_AVD_HOME
 
+#change into tools directory
 cd $MARKER_TOOLS
 
-#install sdk
-wget https://dl.google.com/android/repository/commandlinetools-linux-6858069_latest.zip
+source sdk_lib.bash
+get_commandline_tools
 
-unzip -q commandline*.zip
-rm *.zip
-mkdir "$ANDROID_SDK_ROOT/cmdline-tools"
-mv cmdline-tools tools
-mv tools -t "$ANDROID_SDK_ROOT/cmdline-tools"
-
-#accept licenses
-( yes || true ) | $ANDROID_SDK_ROOT/cmdline-tools/tools/bin/sdkmanager --licenses > /dev/null
-
-#getting latest sdk
-$ANDROID_SDK_ROOT/cmdline-tools/tools/bin/sdkmanager --install "cmdline-tools;latest"
-rm -rf $ANDROID_SDK_ROOT/cmdline-tools/tools  #removing tools directory in orde rto use latest sdk tools, NB: mayb break as new sdk updates and changes arrive
-export_latest_tools_bin
-
-sdkmanager --install "platform-tools" > /dev/null
-sdkmanager --install "build-tools;30.0.3" > /dev/null
-sdkmanager --install "emulator" > /dev/null
-
-export_amulator
-export_platform_tools
-
-#install image package
-sdkmanager --install "system-images;android-28;default;x86_64"
-sdkmanager --install "platforms;android-28" #solves SDK installation not found problem
+source emulator_manager_lib.bash
+install_emulator_image "28" "default" "x86_64"
 
 echo no | avdmanager create avd -f -n emulator1 -c "512M" -k "system-images;android-28;default;x86_64"
 
@@ -45,3 +24,5 @@ emulator @emulator1 -gpu swiftshader_indirect -memory 512 -no-window -no-boot-an
 adb wait-for-device
 
 adb devices
+
+adb -s emulator-5554 shell getprop
