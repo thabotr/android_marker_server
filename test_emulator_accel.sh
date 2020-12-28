2a4a1113f6aff27c22e4614c50104ba94602078d
thabotr <1831661@students.wits.ac.za>
2020-12-28 11:15:28 +0200

    Added unrelated bash script for record.

--- test_emulator_accel.sh
+++ test_emulator_accel.sh
@@ -5,9 +5,9 @@ export ANDROID_SDK_ROOT="$MARKER_TOOLS/android_sdk_linux"
 #avd location
 export ANDROID_AVD_HOME="$ANDROID_SDK_ROOT/.android/avd"
 
-mkdir -p $MARKER_TOOLS $ANDROID_SDK_ROOT $ANDROID_AVD_HOME
+mkdir -p "$MARKER_TOOLS" "$ANDROID_SDK_ROOT" "$ANDROID_AVD_HOME"
 
-cd $MARKER_TOOLS
+cd "$MARKER_TOOLS" || exit 1
 
 #export ANDROID_EMULATOR_HOME="$ANDROID_SDK_ROOT/.android"
 
@@ -16,30 +16,30 @@ cd $MARKER_TOOLS
 #rm -rf "$HOME/.android"
 
 export AVD_LOGS="$ANDROID_AVD_HOME/logs"
-mkdir -p $AVD_LOGS
+mkdir -p "$AVD_LOGS"
 
 #install sdk
 wget https://dl.google.com/android/repository/commandlinetools-linux-6858069_latest.zip
 
 unzip -q commandline*.zip
-rm *.zip
+rm ./*.zip
 mkdir "$ANDROID_SDK_ROOT/cmdline-tools"
 mv cmdline-tools tools
 mv tools -t "$ANDROID_SDK_ROOT/cmdline-tools"
 
-export PATH=$PATH:"$ANDROID_SDK_ROOT/cmdline-tools/tools/bin":"$ANDROID_SDK_ROOT/cmdline-tools/latest/bin" #sdkmanager location, second 'latest' will keep staff working after sdk update
+export PATH="$PATH:$ANDROID_SDK_ROOT/cmdline-tools/tools/bin:$ANDROID_SDK_ROOT/cmdline-tools/latest/bin" #sdkmanager location, second 'latest' will keep staff working after sdk update
 
 #accept licenses
 yes | sdkmanager --licenses > /dev/null
 
 #install emulator for starting avds and platform tools for adb
 sdkmanager "emulator" "platform-tools"> /dev/null
-export PATH=$PATH:"$ANDROID_SDK_ROOT/emulator":"$ANDROID_SDK_ROOT/platform-tools"
+export PATH="$PATH:$ANDROID_SDK_ROOT/emulator:$ANDROID_SDK_ROOT/platform-tools"
 tree ~/
 
 #install image package
 sdkmanager --install "system-images;android-28;default;x86_64"
 
-echo no | avdmanager create avd -f -n emulator1 -c "512M" -k "system-images;android-28;default;x86_64" -p $ANDROID_AVD_HOME
+echo no | avdmanager create avd -f -n emulator1 -c "512M" -k "system-images;android-28;default;x86_64" -p "$ANDROID_AVD_HOME"
 
 emulator @emulator1 -gpu swiftshader_indirect -memory 512 -no-window -no-boot-anim -no-audio -no-snapshot -camera-front none -camera-back none -selinux permissive -no-qt -wipe-data -no-accel
