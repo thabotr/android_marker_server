e6dae8071cf5cd0831047a7410ec8388d348abb0
thabotr <1831661@students.wits.ac.za>
2021-01-15 19:25:30 +0200

    Fixing test fails

--- run_gradle_tests.bash
+++
@@ -1,2 +0,0 @@
-source setup_gradle_env.bash
-./bats/bin/bats tests/test_gradle_setup.bats -T
--- setup_gradle_env.bash
+++
@@ -1,8 +0,0 @@
-source src/gradle_setup.bash
-source set_env_vars.bash
-
-set_var_gradle
-
-install_java
-mkdir -p $GRADLE
-install_gradle $GRADLE
