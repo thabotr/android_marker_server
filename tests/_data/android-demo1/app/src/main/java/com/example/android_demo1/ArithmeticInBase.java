cfcd488ebfab5efa6e06b49a8831764077d1cc0b
thabotr <1831661@students.wits.ac.za>
2021-01-23 12:49:32 +0200

    Added another test to android-demo1 project

---
+++ tests/_data/android-demo1/app/src/main/java/com/example/android_demo1/ArithmeticInBase.java
@@ -0,0 +1,25 @@
+package com.example.android_demo1;
+
+public class ArithmeticInBase
+{
+	public int baseValue ;
+	public ArithmeticInBase( int baseValue)
+	{
+		this.baseValue = baseValue ;
+	}
+	
+	public void validateNumber( int value) throws NumberFormatException
+	{
+		if( value < 0 && value < -1 * baseValue || Math.abs( value) >= this.baseValue)
+		{
+			throw new NumberFormatException( String.format( "Value %d is does not exist in base %d arithmetic.", value, this.baseValue))
+		}
+	}
+	
+	public int add( int num1, int num2) throws NumberFormatException
+	{
+		int sum = num1 + num2 ;
+		//TODO refactor
+		return sum ;
+	}
+}
---
+++ tests/_data/android-demo1/app/src/test/java/com/example/android_demo1/ArithmeticInBaseTest.java
@@ -0,0 +1,56 @@
+package com.example.android_demo1;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.*;
+
+public class ArithmeticInBaseTest
+{
+	ArithmeticInBase aib ;
+	
+	@Rule
+	public ExpectedException thrown = ExpectedException.none() ;
+	
+	@Before
+	public void Setup()
+	{
+		aib = new ArithmeticInBase(6) ;
+	}
+	
+	@Test
+	public void onNegetiveFive_validateNumber_throwsNothing()
+	{
+		aib.validateNumber( -5);
+	}
+	
+	@Test
+	public void onNegetiveSix_validateNumber_throwsNothing()
+	{
+		aib.validateNumber( -6);
+	}
+
+	@Test
+	public void onNumberLessThanNegativeSix_validateNumber_throwsNumberFormatException()
+	{
+		thrown.expect( NumberFormatException.class);
+		aib.validateNumber( -7);
+		aib.validateNumber( -10);
+	}
+	
+	@Test
+	public void onNumberPositiveSix_validateNumber_throwsNumberFormatException()
+	{
+		thrown.expect( NumberFormatException.class);
+		aib.validateNumber( 6);
+	}
+	
+	@Test
+	public void onNumberGreaterThanSix_validateNumber_throwsNumberFormatException()
+	{
+		thrown.expect( NumberFormatException.class);
+		aib.validateNumber( 7);
+		aib.validateNumber( 11);
+	}
+}
