5ff9581c66357449ca7c14b57250155e7ae761d0
thabotr <1831661@students.wits.ac.za>
2021-01-24 00:36:56 +0200

    PREV-logging results array for tests.

--- src/php/Gradler.php
+++ src/php/Gradler.php
@@ -96,6 +96,7 @@ class Gradler
                 array_push( $res_array, json_encode( $xml_object));
             }
         }
+        error_log( "LOGGG : " . json_encode( $res_array));
         return $res_array;
     }
 }
