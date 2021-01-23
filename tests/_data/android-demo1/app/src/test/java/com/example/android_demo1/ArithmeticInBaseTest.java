c1cca62f0bcb886730184c318c379b1f1c2847c9
thabotr <1831661@students.wits.ac.za>
2021-01-23 13:15:03 +0200

    PREV-fixing junit annotation issue with import.

--- tests/_data/android-demo1/app/src/test/java/com/example/android_demo1/ArithmeticInBaseTest.java
+++ tests/_data/android-demo1/app/src/test/java/com/example/android_demo1/ArithmeticInBaseTest.java
@@ -2,6 +2,7 @@ package com.example.android_demo1;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.Rule ;
 import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.*;
