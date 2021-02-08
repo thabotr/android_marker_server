dbe6c3a869d7dfd4cb1cfcb8dc0f26c97f73ff53
thabotr <1831661@students.wits.ac.za>
2021-02-08 03:55:40 +0200

    PREV-downed count report to 25 rounds.

--- src/bash/emulator_manager_lib.bash
+++ src/bash/emulator_manager_lib.bash
@@ -19,9 +19,8 @@ wait_for_services()
 	count=0
 	while ! ( adb -s $emulator_serial shell service list | grep "Found $2 services" -q ) ;
 	do
-		echo "Count is $count".
 		count=$(( $count+1 ))
-		if [ $(( $count % 40 )) == 0 ];
+		if [ $(( $count % 25 )) == 0 ];
 		then
 			echo "Waiting for services of device $1."
 		fi
